d1ba3fc24458a9cfd459f9c6689148fdac35fb67
Corné den Breejen <cornedev@outlook.com>
2021-11-09 09:05:01 +0100

    Added more up to date titleLayer

--- src/main.ts
+++ src/main.ts
@@ -24,13 +24,8 @@ const markerGroup = L.layerGroup()
 // Disable double click for the event
 map.doubleClickZoom.disable();
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: '' // Add your mapbox token here
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
 const onMapClick = (e: { latlng: L.LatLngExpression; }) => {
